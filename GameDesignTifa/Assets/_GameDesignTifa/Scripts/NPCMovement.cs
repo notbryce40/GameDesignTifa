66ca5ef1417cebaba68237bd7d0885bbf171b279
finalfantasynerd <128729159+finalfantasynerd@users.noreply.github.com>
2024-03-18 09:35:11 -0500

    AI Pathing Complete (For One Customer)

--- GameDesignTifa/Assets/_GameDesignTifa/Scripts/NPCMovement.cs
+++ GameDesignTifa/Assets/_GameDesignTifa/Scripts/NPCMovement.cs
@@ -1,33 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NPCMovement : MonoBehaviour
 {
-    public GameObject theDestination;
-    public GameObject finalDestination;
-    public GameObject NpcCat;
-    public int pivotPoint = 0;
-    NavMeshAgent theAgent;
+    public GameObject spawnCube;
+    public GameObject registerCube;
+    public GameObject finalDestinationCube;
+    public GameObject NpcCatPrefab;
 
+    private bool orderCompleted = false;
+    private NavMeshAgent theAgent;
+    private GameObject npcCatInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        theAgent = GetComponent<NavMeshAgent>();
-        theAgent.SetDestination(theDestination.transform.position);
+        // Instantiate the NPC from the prefab at the spawnCube's position
+        npcCatInstance = Instantiate(NpcCatPrefab, spawnCube.transform.position, Quaternion.identity);
+        
+        // Get the NavMeshAgent component from the instantiated NPC
+        theAgent = npcCatInstance.GetComponent<NavMeshAgent>();
+        
+        // Set the first destination to the registerCube
+        theAgent.SetDestination(registerCube.transform.position);
     }
 
-    void OnCollisionEnter(Collision col)
+    // Update is called once per frame
+    void Update()
     {
-        if (col.gameObject.name == "Destination Cube")
+        // Check the distance to the registerCube and if the order is not completed
+        if (!orderCompleted && Vector3.Distance(npcCatInstance.transform.position, registerCube.transform.position) < 1.0f)
         {
-            theAgent.SetDestination(finalDestination.transform.position);
+            // Logic to create an order can be added here
+            orderCompleted = true;
+
+            // Set the destination to the finalDestinationCube
+            theAgent.SetDestination(finalDestinationCube.transform.position);
         }
-        if (col.gameObject.name == "FinalDestinationCube")
+
+        // Check if the NPC has reached the finalDestinationCube
+        if (orderCompleted && Vector3.Distance(npcCatInstance.transform.position, finalDestinationCube.transform.position) < 1.0f)
         {
-            Destroy(NpcCat);
+            // Despawn the NPC
+            Destroy(npcCatInstance);
         }
     }
 }
