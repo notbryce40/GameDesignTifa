07c766188c174d78a9e5567bc6547e93fe59e58b
IronBanes <gavinator0000@gmail.com>
2024-03-17 23:24:40 -0500

    Update PlayerMovement.cs

--- GameDesignTifa/Assets/_GameDesignTifa/Scripts/PlayerMovement.cs
+++ GameDesignTifa/Assets/_GameDesignTifa/Scripts/PlayerMovement.cs
@@ -13,12 +13,11 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 moveDirections;
 
     //Refs
-    private CharacterController controller;
+   
     private Animator anim;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
         anim = GetComponentInChildren<Animator>();
         
     }
@@ -28,19 +27,21 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
-    private void Move()
+    /*private void Move()
     {
-        float moveZ = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        moveDirections = new Vector3 (0, 0, moveZ);
+        moveDirections = new Vector3(horizontalInput, 0f,verticalInput).normalized;
         moveDirections = transform.TransformDirection(moveDirections);
 
         
         if(moveDirections != Vector3.zero && isHoldingBox == false )
         {
+            transform.rotation = Quaternion.LookRotation(moveDirections);
             Walk();
         }
-        else if(moveDirections != Vector3.zero && isHoldingBox == false)
+        else if(moveDirections == Vector3.zero && isHoldingBox == false)
         {
             Idle();
         }
@@ -50,14 +51,52 @@ public class PlayerMovement : MonoBehaviour
         }
         else if(moveDirections != Vector3.zero && isHoldingBox == true )
         {
+            transform.rotation = Quaternion.LookRotation(moveDirections);
             walkbox();
         }
 
         moveDirections *= moveSpeed;
 
-        controller.Move(moveDirections* Time.deltaTime);
+        transform.Translate(moveDirections * moveSpeed * Time.deltaTime, Space.World);
 
+    }*/
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        moveDirections = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (moveDirections != Vector3.zero)
+        {
+            // Rotate the player based on horizontal input
+            float targetAngle = Mathf.Atan2(moveDirections.x, moveDirections.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            // Determine whether to walk or walk while holding a box
+            if (!isHoldingBox)
+                Walk();
+            else
+                walkbox();
+        }
+        else
+        {
+            if (isHoldingBox)
+            {
+                // If no movement input, stop walking
+                holdbox();
+            }
+            else
+            {
+                Idle();
+            }
+            
+        }
+
+        moveDirections *= moveSpeed;
+        transform.Translate(moveDirections * Time.deltaTime, Space.World);
     }
+
     private void Idle()
     {
         anim.SetBool("isWalking", false); 
